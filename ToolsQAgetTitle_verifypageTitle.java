fb57e5a556978e022a29271afb23d12c1fd64ca3
rekhapal1998 <142340116+rekhapal1998@users.noreply.github.com>
2023-08-24 00:37:45 +0530

    Update and rename a.java to ToolsQAgetTitle_verifypageTitle.java
    This is the Selenium Automation Script to get the page title of ToolsQA Demo website and verification of the page title.

---
+++ ToolsQAgetTitle_verifypageTitle.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Practice {
+//code to print title of the page and verify the title
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\BrowserDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demoqa.com/");
+		String title = driver.getTitle();
+		System.out.println(title);
+		if (title.equals("DEMOQA")) {
+			System.out.println("title matched");
+		} else {
+			System.out.println("title not matched");
+		}
+
+		// OR
+
+		// System.out.println(driver.getTitle());
+
+		// String expectedTitle = "Demo";
+		// String title = driver.getTitle();
+		// if (title.equalsIgnoreCase(expectedTitle)) {
+		// System.out.println("Title Matched");
+		// } else {
+		// System.out.println("Not a match");
+		// }
+	}
+
+}
+
